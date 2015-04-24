884a9f4bab0186b109f35d5c833f5ce11a612342
wayne2k <wayne2k@gmail.com>
2015-04-24 17:28:13 +0200

    Code Orginization
    Start Function code moved into a new function.
    Looks cleaner.
    Added a Marsh Area

--- Assets/Scripts/TileMap.cs
+++ Assets/Scripts/TileMap.cs
@@ -10,10 +10,17 @@ public class TileMap : MonoBehaviour {
 	int mapSizeX = 10;
 	int mapSizeY = 10;
 
-	void Start() {
+	void Start() 
+	{
+		GenerateMapData ();
+		GenerateMapVisual();
+	}
+
+	void GenerateMapData ()
+	{
 		// Allocate our map tiles
 		tiles = new int[mapSizeX,mapSizeY];
-
+		
 		// Initialize our map tiles to be grass
 		for(int x=0; x < mapSizeX; x++) {
 			for(int y=0; y < mapSizeY; y++) {
@@ -21,32 +28,35 @@ public class TileMap : MonoBehaviour {
 			}
 		}
 
-		// Let's make a u-shaped mountain range
+		// Make Big Swamp area
+		for(int x=3; x <=5 ; x++) {
+			for(int y=0; y < 4; y++) {
+				tiles[x,y] = 1;
+			}
+		}
 
+		// Let's make a u-shaped mountain range
 		tiles[4, 4] = 2;
 		tiles[5, 4] = 2;
 		tiles[6, 4] = 2;
 		tiles[7, 4] = 2;
 		tiles[8, 4] = 2;
-
+		
 		tiles[4, 5] = 2;
 		tiles[4, 6] = 2;
 		tiles[8, 5] = 2;
 		tiles[8, 6] = 2;
-
-		// Now that we have all of our map data, let's spawn the visual prefabs
-
-		GenerateMapVisual();
 	}
 
-	void GenerateMapVisual() {
-		for(int x=0; x < mapSizeX; x++) {
-			for(int y=0; y < mapSizeX; y++) {
+	void GenerateMapVisual()
+	{
+		for(int x=0; x < mapSizeX; x++) 
+		{
+			for(int y=0; y < mapSizeX; y++) 
+			{
 				TileType tt = tileTypes[ tiles[x,y] ];
 				Instantiate( tt.tileVisualPrefab, new Vector3(x, y, 0), Quaternion.identity );
 			}
 		}
-
 	}
-
 }
