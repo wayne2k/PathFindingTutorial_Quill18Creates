a5c8d004c81715b4c1c7a97c3d735eb7304d3920
wayne2k <wayne2k@gmail.com>
2015-04-24 20:35:17 +0200

    Pathfinding Logic fixed. Debug drawlines added

--- Assets/Scripts/TileMap.cs
+++ Assets/Scripts/TileMap.cs
@@ -15,6 +15,11 @@ public class TileMap : MonoBehaviour {
 	int mapSizeY = 10;
 	
 	void Start() {
+
+		selectedUnit.GetComponent<Unit> ().tileX = (int) selectedUnit.transform.position.x;
+		selectedUnit.GetComponent<Unit> ().tileY = (int) selectedUnit.transform.position.y;
+		selectedUnit.GetComponent<Unit> ().map = this;
+
 		GenerateMapData();
 		GeneratePathfindingGraph();
 		GenerateMapVisual();
@@ -63,9 +68,12 @@ public class TileMap : MonoBehaviour {
 			neighbours = new List<Node>();
 		}
 
-		public float DistanceToNode (Node n)
-		{
-			return Vector2.Distance ( new Vector2 (x, y), new Vector2 (n.x, n.y));
+		public float DistanceTo(Node n) 
+		{	
+			return Vector2.Distance(
+				new Vector2(x, y),
+				new Vector2(n.x, n.y)
+				);
 		}
 	}
 	
@@ -120,93 +128,90 @@ public class TileMap : MonoBehaviour {
 		return new Vector3(x, y, 0);
 	}
 	
-	public void GeneratePathTo(int x, int y) 
-	{
-		selectedUnit.GetComponent <Unit> ().currentPath = null;
-			
+	public void GeneratePathTo(int x, int y) {
+		// Clear out our unit's old path.
+		selectedUnit.GetComponent<Unit>().currentPath = null;
+		
 		Dictionary<Node, float> dist = new Dictionary<Node, float>();
 		Dictionary<Node, Node> prev = new Dictionary<Node, Node>();
-
-		// Setup the "Q" List of nodes we havent checked yet.
-		List <Node> unvisited = new List<Node> (); 
-
+		
+		// Setup the "Q" -- the list of nodes we haven't checked yet.
+		List<Node> unvisited = new List<Node>();
+		
 		Node source = graph[
-		                    selectedUnit.GetComponent<Unit>().tileX = x, 
-		                    selectedUnit.GetComponent<Unit>().tileY = y
+		                    selectedUnit.GetComponent<Unit>().tileX, 
+		                    selectedUnit.GetComponent<Unit>().tileY
 		                    ];
-
-		Node target = graph[x, y];
-
-		dist [source] = 0;
-		prev [source] = null;
-
-
-		//Initilize everyting to have infinity disatance.
-		//since we dont know any better right now. its possible some nodes cant be reached from the source
-		// which should make INFINITY a reasonable value.
-		foreach (Node v in graph) 
-		{
-			if (v != source) {
+		
+		Node target = graph[
+		                    x, 
+		                    y
+		                    ];
+		
+		dist[source] = 0;
+		prev[source] = null;
+		
+		// Initialize everything to have INFINITY distance, since
+		// we don't know any better right now. Also, it's possible
+		// that some nodes CAN'T be reached from the source,
+		// which would make INFINITY a reasonable value
+		foreach(Node v in graph) {
+			if(v != source) {
 				dist[v] = Mathf.Infinity;
 				prev[v] = null;
 			}
-
-			unvisited.Add (v);
+			
+			unvisited.Add(v);
 		}
-
-		while (unvisited.Count > 0)
-		{
-//			Node u = unvisited.OrderBy (n => dist[n]).First ();
+		
+		while(unvisited.Count > 0) {
 			// "u" is going to be the unvisited node with the smallest distance.
 			Node u = null;
-
-			foreach (Node possibleU in unvisited)
-			{
-				if (u == null || dist[possibleU] < dist[u])
-				{
+			
+			foreach(Node possibleU in unvisited) {
+				if(u == null || dist[possibleU] < dist[u]) {
 					u = possibleU;
 				}
 			}
-
-			if (u == target) {
-				break;
+			
+			if(u == target) {
+				break;	// Exit the while loop!
 			}
-
-			unvisited.Remove (u);
-
-			foreach (Node v in u.neighbours)
-			{
-				float alt = dist[u] + u.DistanceToNode (v);
-				if (alt < dist[v])
-				{
+			
+			unvisited.Remove(u);
+			
+			foreach(Node v in u.neighbours) {
+				float alt = dist[u] + u.DistanceTo(v);
+				if( alt < dist[v] ) {
 					dist[v] = alt;
 					prev[v] = u;
 				}
 			}
 		}
-
-		if (prev[target] == null)
-		{
-			// Not route between target and source;
+		
+		// If we get there, the either we found the shortest route
+		// to our target, or there is no route at ALL to our target.
+		
+		if(prev[target] == null) {
+			// No route between our target and the source
 			return;
 		}
-
-
-		List<Node> currentPath = new List<Node> ();
+		
+		List<Node> currentPath = new List<Node>();
+		
 		Node curr = target;
-
-
-		//Step through prev chain and add to our path.
-		while (curr != null) {
-			currentPath.Add (curr);
+		
+		// Step through the "prev" chain and add it to our path
+		while(curr != null) {
+			currentPath.Add(curr);
 			curr = prev[curr];
 		}
-
-		//Right now currentPath describes a path from our target to out source
-		//so we need to invert it.
-
-		currentPath.Reverse ();
-
-		selectedUnit.GetComponent <Unit> ().currentPath = currentPath;
+		
+		// Right now, currentPath describes a route from out target to our source
+		// So we need to invert it!
+		
+		currentPath.Reverse();
+		
+		selectedUnit.GetComponent<Unit>().currentPath = currentPath;
 	}
 }
--- Assets/Scripts/Unit.cs
+++ Assets/Scripts/Unit.cs
@@ -5,6 +5,26 @@ public class Unit : MonoBehaviour {
 
 	public int tileX;
 	public int tileY;
+	public TileMap map;
 
 	public List<TileMap.Node> currentPath = null;
+
+	void Update ()
+	{
+		if (currentPath != null) {
+			int currentNode = 0;
+
+			while (currentNode < currentPath.Count -1)
+			{
+				Vector3 start = map.TileCoordToWorldCoord (currentPath[currentNode].x, currentPath[currentNode].y);
+				Vector3 end   = map.TileCoordToWorldCoord (currentPath[currentNode + 1].x, currentPath[currentNode + 1].y);;
+
+				start += Vector3.forward * -1f;
+				end += Vector3.forward * -1f;
+
+				Debug.DrawLine (start, end, Color.red);
+				currentNode ++;
+			}
+		}
+	}
 }
